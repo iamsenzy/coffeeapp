df71e7974740e5f8f7f401f29869c1393da8b866
Geszti Bence <geszti.bence@codeyard.eu>
2021-04-04 13:00:55 +0200

    Initial Commit

---
+++ CoffeeAppSwiftUI/CoffeeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppSwiftUIApp.swift
+//  CoffeeAppSwiftUI
+//
+//  Created by Geszti Bence on 2021. 04. 04..
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
